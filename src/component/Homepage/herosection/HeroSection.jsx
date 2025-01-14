6021ac4138eea4d61627a60ea1a9b05f1baf2ca9
Bhushankakade <kakadebhushan26@gmail.com>
2025-01-14 15:30:29 +0530

    loop the image

--- src/component/Homepage/herosection/HeroSection.jsx
+++ src/component/Homepage/herosection/HeroSection.jsx
@@ -1,28 +1,47 @@
-import "./HeroSection.css";
+import React, { useEffect, useRef } from "react";
 import heroImage from "../../../../src/assets/heroimage.mp4";
+import "./HeroSection.css";
 
-const home = () => {
+const Home = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (video) {
+      video.addEventListener("ended", () => {
+        video.currentTime = 0; // Rewind
+        video.play(); // Start playing again
+      });
+    }
+    return () => {
+      if (video) video.removeEventListener("ended", () => {});
+    };
+  }, []);
+
   return (
-    <>
-      <div className="landing-intro">
-        <div className="ion__landingintro-img">
-          <video src={heroImage} autoPlay loop className="heroimage" />
-        </div>
-
-        <div className="landing__intro-section">
-          <h1>
-            Empowering Consultants, <br /> Connecting Companies
-          </h1>
-          <p>
-            Catalystion brings together expert consultants and ambitious
-            businesses to solve critical challenges and drive innovation. Our
-            platform connects companies with top professionals for fast,
-            efficient solutions.
-          </p>
-        </div>
+    <div className="landing-intro">
+      <div className="ion__landingintro-img">
+        <video
+          ref={videoRef}
+          src={heroImage}
+          autoPlay
+          muted
+          className="heroimage"
+        />
       </div>
-    </>
+      <div className="landing__intro-section">
+        <h1>
+          Empowering Consultants, <br /> Connecting Companies
+        </h1>
+        <p>
+          Catalystion brings together expert consultants and ambitious
+          businesses to solve critical challenges and drive innovation. Our
+          platform connects companies with top professionals for fast, efficient
+          solutions.
+        </p>
+      </div>
+    </div>
   );
 };
 
-export default home;
+export default Home;
